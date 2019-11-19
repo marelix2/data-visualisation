f78ef1d75ce51dc4613f2d9cb6ddd581dfe085c8
Michał Rusinek <marelix2@gmail.com>
2019-11-19 02:44:44 +0100

    Refactor of Pixel Displayer

--- src/components/PixelDisplayer/PixelDisplayer.jsx
+++ src/components/PixelDisplayer/PixelDisplayer.jsx
@@ -1,49 +1,76 @@
 import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 import PhoneBackgorund from './components/PhoneBackgorund'
+import utils from './utils'
+import {chunk} from 'lodash'
 import COLORS from './../../constants/colors'
 
-const drawIcon =(canvas, posX, posY)  => {
-
-    canvas.fillStyle = posX % 2 ? COLORS.ORANGE : COLORS.GREEN
-    canvas.beginPath();
-    canvas.arc(posX, posY, 24, 0, 2 * Math.PI, false);
-    canvas.fill();
-
-   canvas.fillStyle = '#000'
-   const img = new Image()
-
-   img.width=150
-   img.onload = () => {
-       canvas.drawImage(img,posX - 13,posY - 12, 25, 25)
-   } 
-   img.src=require('./../../dist/svg/earth.svg')
-
-   canvas.fillStyle = COLORS.TEXT_PIXEL
-   canvas.font='10px Roboto'
-   canvas.fillText('Kategoria', posX - 21, posY + 50)
+const styles = {
+    canvasWrapper : {
+        border:'1px solid #444',
+     borderRadius: 30,
+      position: 'absolute',
+       left: 530, 
+       top: 64}
 }
 
-const drawRow = (canvas, posX, posY) => {
-   ['', '', '', '', ''].map((el, index) => drawIcon(canvas, posX + index * 48 + index * 14, posY))
-}
-
-const fillBackbgorund = canvas => {
-   canvas.fillStyle = '#ddd'
-   canvas.fillRect(0,0,340,700);
-}
 
 
 
  const PixelDisplayer = ({phoneWidth, phoneHeight, backgorundFill}) => {
-     const defaultRowProps = {
-        size:90,
-        space:28,
-        rx: '50%',
-        ry:'50%',
-        textXOffset: 32,
-        textYOffsetL: 120,
-     }
+ 
+     const items = [
+        {category: 'earth',
+           number:'33',
+           imgSrc: 'earth.svg',
+           radius: '20',
+           color: COLORS.ORANGE_2},
+           {category: 'utils',
+           number:'24',
+           imgSrc: 'edit.svg'},
+           {category: 'utils',
+           number:'22',
+           imgSrc: 'feature.svg'},
+           {category: 'dev',
+           number:'1',
+           imgSrc: 'settings.svg',
+           radius: '18',
+       },
+           {category: 'features',
+           number:'14',
+           imgSrc: 'console.svg',
+           radius: '20',
+           color: COLORS.RED},
+           {category: 'utils',
+           number:'22',
+           imgSrc: 'earth.svg',
+           color: COLORS.ORANGE},
+           {category: 'earth',
+           number:'33',
+           imgSrc: 'earth.svg',
+           radius: '20',
+           color: COLORS.ORANGE_2},
+           {category: 'utils',
+           number:'24',
+           imgSrc: 'edit.svg'},
+           {category: 'utils',
+           number:'22',
+           imgSrc: 'feature.svg'},
+           {category: 'dev',
+           number:'1',
+           imgSrc: 'settings.svg',
+           radius: '18',
+       },
+           {category: 'features',
+           number:'14',
+           imgSrc: 'console.svg',
+           radius: '20',
+           color: COLORS.RED},
+           {category: 'utils',
+           number:'22',
+           imgSrc: 'earth.svg',
+           color: COLORS.ORANGE},
+       ]
 
      const [canvas, setCanvas] = useState(null)
      
@@ -53,12 +80,13 @@ const fillBackbgorund = canvas => {
 
      useEffect(() => {
         if(canvas) { 
-        fillBackbgorund(canvas)
-
-        drawRow(canvas, 40, 120)
-        drawRow(canvas, 40, 220)
-        drawRow(canvas, 40, 320)
-        drawRow(canvas, 40, 420)
+        
+        utils.fillBackbgorund(canvas)
+        
+        const chunks = chunk(items, 5)
+            chunks.forEach((ch, index ) => {
+                utils.drawRow(canvas, ch,  40, utils.getIconPosYOffset(index))
+            })
         }
      }, [canvas])
 
@@ -73,7 +101,11 @@ const fillBackbgorund = canvas => {
         <rect id="rect4154" fillRule="evenodd" rx="25" ry="25" height="60" width="600" y="160" x="42" fill="#fff"/>
         </svg>
 
-        <canvas id='pixelCanvas' width='335' height='700' style={{border:'1px solid #444', borderRadius: 30, position: 'absolute', left: 530, top: 64}}></canvas>
+        <canvas
+         id='pixelCanvas' 
+        width='335'
+         height='700' 
+         style={styles.canvasWrapper}/>
         </>
     )
 }
