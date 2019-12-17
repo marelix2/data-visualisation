fd7c2d80fa1b3d16b121cff02e9509aa4d4c8e96
Michał Rusinek <marelix2@gmail.com>
2019-12-17 04:09:28 +0100

    7: cost color visualisation impl

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,71 @@
-export const getCategories = (items) =>  [...new Set(items.map(item  => item.category))].filter(_ => _)
+import COLORS from './../constants/colors'
+import _ from 'lodash'
 
-export const getItems = (items, env) => items.filter( item => item.environment === env)
+export const getCategories = (items) => [...new Set(items.map(item => item.category))].filter(_ => _)
 
-const getSupportedDevices = (items, category) => {
-    const supportedDevices = items.filter( item => item.category === category)
-    return  Math.floor(supportedDevices.reduce((acc, {numberOfSupportedDevices}) => acc + parseInt(numberOfSupportedDevices), 0) / supportedDevices.length)
+export const getItems = (items, env) => items.filter(item => item.environment === env)
+
+const getSupportedDevicesPercentage = (items, category, maxValue) => {
+    const supportedDevices = items.filter(item => item.category === category)
+    const avg = Math.floor(supportedDevices.reduce((acc, { numberOfSupportedDevices }) => acc + parseInt(numberOfSupportedDevices), 0) / supportedDevices.length)
+
+    return Math.floor(avg / maxValue * 100)
+}
+
+const getSupportedDevicesPercentageAndroid = (items, category,supportDivicesAndroid) => {
+    let supportedDevices = items.filter(item => item.category === category).map(d => d.numberOfSupportedDevices).sort((a, b) => a - b)
+    const occurances = {}
+    supportedDevices.forEach(a => {
+        if (occurances[a] !== undefined) occurances[a] += 1
+        else occurances[a] = 0
+    })
+    const keys = Object.keys(occurances)
+    let max = 0
+    let mostPopularVersion
+
+    keys.forEach(key => {
+        if (max < occurances[key]){
+            max = occurances[key]
+            mostPopularVersion = key
+        } 
+    })
+    let androidKeys = Object.keys(supportDivicesAndroid)
+    androidKeys = androidKeys.slice(androidKeys.indexOf(mostPopularVersion))
+    return Math.floor(androidKeys.reduce( (result, key) => result + supportDivicesAndroid[key], 0))
+}
+
+const getAvergeCostOfApp = (items, category, supportedVersions) => {
+    const categoryItems = items.filter(item => item.category === category)
+    return Math.floor(categoryItems.reduce((acc, { price }) => acc + parseFloat(price), 0) / categoryItems.length)
+}
+
+const getMaxAppPrice = (itmes, categories) => {
+    let max = 0
+    categories.forEach(category => {
+        const avergeCostOfApp = getAvergeCostOfApp(itmes, category)
+        if (max < avergeCostOfApp) max = avergeCostOfApp
+    })
+    return max
+}
+
+const getMinAppPrice = (itmes, categories) => {
+    let min = 0
+    categories.forEach(category => {
+        const avergeCostOfApp = getAvergeCostOfApp(itmes, category)
+        if (min >= avergeCostOfApp) min = avergeCostOfApp
+    })
+    return min
+}
+
+const getColorByPrice = (min, max, price) => {
+    const firstCentile = (max - min) * 0.25
+    const secondCentile = (max - min) * 0.5
+    const trindCentile = (max - min) * 0.75
+
+    if (firstCentile > price) return COLORS.GREEN
+    if (firstCentile <= price && secondCentile > price) return COLORS.YELLOW
+    if (secondCentile <= price && trindCentile > price) return COLORS.ORANGE
+    if (trindCentile <= price) return COLORS.RED
 }
 
 const parseCategoryName = category => `${category.split(' ').join('')}.svg`
@@ -13,33 +74,63 @@ export const getIphoneMainPageItems = (items) => {
     const newItems = getItems(items, 'iOS')
     const categories = getCategories(newItems)
     const out = []
-    categories.forEach( category => {
-        const supportedDivicesAvg = getSupportedDevices(newItems, category)
+
+    const maxPrice = getMaxAppPrice(newItems, categories)
+    const minPrice = getMinAppPrice(newItems, categories)
+    categories.forEach(category => {
+        const supportedDivicesAvg = getSupportedDevicesPercentage(newItems, category, 45)
+        const avergeCostOfApp = getAvergeCostOfApp(newItems, category)
         out.push({
             category,
             number: supportedDivicesAvg,
             imgSrc: parseCategoryName(category),
+            color: getColorByPrice(minPrice, maxPrice, avergeCostOfApp)
         })
     })
 
-    return out   
+    return out
 }
 
 export const getAndroidMainPageItems = (items) => {
     const newItems = getItems(items, 'Android')
     const categories = getCategories(newItems)
     const out = []
-    categories.forEach( category => {
-        // const supportedDivicesAvg = getSupportedDevices(newItems, category)
+
+    const maxPrice = getMaxAppPrice(newItems, categories)
+    const minPrice = getMaxAppPrice(newItems, categories)
+    categories.forEach(category => {
+        const avergeCostOfApp = getAvergeCostOfApp(newItems, category)
+        const supportedDivicesAvg = getSupportedDevicesPercentageAndroid(newItems, category, supportDivicesAndroid)
         out.push({
             category,
-            number: 99,
+            number: supportedDivicesAvg,
             imgSrc: parseCategoryName(category),
+            color: getColorByPrice(minPrice, maxPrice, avergeCostOfApp)
         })
     })
 
-    return out   
+    return out
 }
+
+//data from https://developer.android.com/about/dashboards/index.html 17.12.19
+const supportDivicesAndroid = {
+    '2.3': 0.3,
+    '4.0': 0.3,
+    '4.1': 1.2,
+    '4.2': 1.5,
+    '4.3': 0.5,
+    '4.4': 6.9,
+    '5.0': 3.0,
+    '5.1': 11.5,
+    '6.0': 16.9,
+    '7.0': 11.4,
+    '7.1': 7.8,
+    '8.0': 12.9,
+    '8.1': 15.4,
+    '9.0': 10.4,
+}
+
+
 
 /*
   // const items = [
